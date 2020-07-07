2dd20914bf34a3ba09bad973d63bbb0532f6d939
hustin-paul <hj.paul7@gmail.com>
2020-07-07 18:10:15 -0400

    logout button and sidebar scrollbar fix

--- src/home/Navbar.tsx
+++ src/home/Navbar.tsx
@@ -1,1 +1,44 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+type acceptedProps = {
+  clearToken: any;
+};
+
+export default class Navbar extends React.Component<acceptedProps, {}> {
+  constructor(props: acceptedProps) {
+    super(props);
+  }
+  logoutBtn() {
+    return localStorage.getItem("token") === null ? (
+      ""
+    ) : (
+      <Button
+        onClick={this.props.clearToken}
+        color="inherit"
+        id="navLog"
+        style={{ marginLeft: "90vw" }}
+      >
+        Logout
+      </Button>
+    );
+  }
+
+  render() {
+    return (
+      <AppBar position="static">
+        <Toolbar className="classes.color">
+          <Typography
+            variant="h6"
+            id="navTitle"
+            className="classes.title"
+          ></Typography>
+          {this.logoutBtn()}
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
